93a26f95b2dba753eb1f48e16e64003a1c5a9dc0
Anna Smolentzov <annasmolentzov@gmail.com>
2019-01-02 20:07:34 -0700

    Adds coupon model test basic validations

---
+++ spec/models/coupon_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe Coupon, type: :model do
+  describe 'Validations' do
+    it { should validate_presence_of(:coupon_type) }
+    it { should validate_presence_of(:amount) }
+    it { should validate_presence_of(:code) }
+    it { should validate_uniqueness_of(:code) }
+  end
+end
