f4bc8a4f1e3eb248ac747d64136cdf29b7c3400c
Anna Smolentzov <annasmolentzov@gmail.com>
2019-01-03 11:54:23 -0700

    Adds cart model tests for adding coupons

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -94,4 +94,40 @@ RSpec.describe Cart do
 
     expect(cart.grand_total).to eq(cart.subtotal(item_1.id) + cart.subtotal(item_2.id))
   end
+  
+  it '.add_coupon' do
+    item_1 = create(:item)
+    cart = Cart.new({})
+    cart.add_item(item_1.id)
+  
+    coupon = create(:percent_coupon)
+    cart.add_coupon(coupon)
+    
+    expect(cart.coupons).to eq([coupon])
+    
+    coupon_2 = create(:dollar_coupon)
+    coupon_3 = create(:dollar_coupon)
+    cart.add_coupon(coupon_2)
+    cart.add_coupon(coupon_3)
+    
+    expect(cart.coupons).to eq([coupon, coupon_2, coupon_3])
+  end
+  
+  it '.coupons' do
+      item_1 = create(:item)
+      cart = Cart.new({})
+      cart.add_item(item_1.id)
+    
+      coupon = create(:percent_coupon)
+      cart.add_coupon(coupon)
+      
+      expect(cart.coupons).to eq([coupon])
+      
+      coupon_2 = create(:dollar_coupon)
+      coupon_3 = create(:dollar_coupon)
+      cart.add_coupon(coupon_2)
+      cart.add_coupon(coupon_3)
+      
+      expect(cart.coupons).to eq([coupon, coupon_2, coupon_3])
+  end
 end
