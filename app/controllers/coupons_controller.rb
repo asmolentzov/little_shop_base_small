9aa08b618dbca7b48c82f1d13d1f033f353e2a42
Anna Smolentzov <annasmolentzov@gmail.com>
2019-01-02 20:24:51 -0700

    Adds coupons index page

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,9 +1,21 @@
 class CouponsController < ApplicationController
+  def index
+    @coupons = Coupon.merchant_coupons(current_user)
+  end
+  
   def new
     @coupon = Coupon.new
   end
   
   def create
-    
+    coupon = Coupon.new(coupon_params)
+    coupon.save
+    redirect_to coupons_path
+  end
+  
+  private
+  
+  def coupon_params
+    params.require(:coupon).permit(:coupon_type, :amount, :cart_minimum, :code)
   end
 end
