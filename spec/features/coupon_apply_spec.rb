c15285d285edb0fd21e33fdf82d029f4f23531f5
Anna Smolentzov <annasmolentzov@gmail.com>
2019-01-03 17:25:31 -0700

    Adds feature test for applying an invalid coupon code

--- spec/features/coupon_apply_spec.rb
+++ spec/features/coupon_apply_spec.rb
@@ -120,4 +120,37 @@ describe 'Coupon apply workflow' do
       expect(page).to have_content("Coupon #{@coupon_2.code} was successfully applied!")
     end
   end
+  
+  describe 'I cannot apply an invalid coupon code' do
+    before(:each) do
+      @merchant = create(:merchant)
+      @item = create(:item, user: @merchant)
+      @coupon = create(:percent_coupon, user: @merchant)
+    end
+    scenario 'as a visitor' do
+      visit item_path(@item)
+    end
+    scenario 'as a registered user' do
+      customer = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(customer)
+      
+      visit item_path(@item)
+    end
+    
+    after(:each) do
+      click_button "Add to Cart"
+      
+      visit cart_path
+      
+      wrong_code = 'WRONG'
+      fill_in :coupon_code, with: wrong_code
+      click_button "Apply Coupon"
+      
+      expect(current_path).to eq(cart_path)
+      expect(page).to have_content("Coupon #{wrong_code} is not a valid coupon")
+      
+      expect(page).to have_content('Add Coupon')
+      expect(page).to have_content("Grand Total: #{number_to_currency(@item.price)}")
+    end
+  end
 end
