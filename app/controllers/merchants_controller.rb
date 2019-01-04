c0112b1bc0e68101f8e1eb66d3da9a5e469a12c2
Anna Smolentzov <annasmolentzov@gmail.com>
2019-01-04 11:05:41 -0700

    Adds placeholder images to view and controller

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -18,6 +18,7 @@ class MerchantsController < ApplicationController
 
   def show
     @merchant = current_user
+    @placeholder_image_items = @merchant.my_placeholder_image_items
     @orders = @merchant.my_pending_orders
     @top_5_items = @merchant.top_items_by_quantity(5)
     @qsp = @merchant.quantity_sold_percentage
