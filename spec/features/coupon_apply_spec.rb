37d0390d3663e90165de536b644c5891020f4867
Anna Smolentzov <annasmolentzov@gmail.com>
2019-01-03 17:01:17 -0700

    Adds feature test for removing a coupon

--- spec/features/coupon_apply_spec.rb
+++ spec/features/coupon_apply_spec.rb
@@ -70,4 +70,51 @@ describe 'Coupon apply workflow' do
       expect(page).to_not have_button("Apply Coupon")
     end
   end
+  
+  describe 'I can remove a coupon code then apply a new one' do
+    before(:each) do
+      @merchant = create(:merchant)
+      @item = create(:item, user: @merchant)
+      @coupon = create(:percent_coupon, user: @merchant)
+      @coupon_2 = create(:dollar_coupon, user: @merchant)
+    end
+    scenario 'as a visitor' do
+      visit item_path(@item)
+    end
+    scenario 'as a registered user' do
+      customer = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(customer)
+      
+      visit item_path(@item)
+    end
+    
+    after(:each) do
+      click_button "Add to Cart"
+      
+      visit cart_path
+      
+      fill_in :coupon_code, with: @coupon.code
+      click_button "Apply Coupon"
+      
+      expect(current_path).to eq(cart_path)
+      expect(page).to have_content("Coupon #{@coupon.code} was successfully applied!")
+      
+      expect(page).to_not have_content("Add Coupon")
+      expect(page).to_not have_button("Apply Coupon")
+      
+      expect(page).to have_link('Remove Coupon')
+      click_link('Remove Coupon')
+      
+      expect(current_path).to eq(cart_path)
+      expect(page).to_not have_content(@coupon.code)
+      expect(page).to have_content('Add Coupon')
+      expect(page).to_not have_link('Remove Coupon')
+      
+      fill_in :coupon_code, with: @coupon_2.code
+      click_button 'Apply Coupon'
+      
+      expect(current_path).to eq(cart_path)
+      expect(page).to have_content("Coupon #{@coupon.code} was successfully applied!")
+    end
+  end
 end
